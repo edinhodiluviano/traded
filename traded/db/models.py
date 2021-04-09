d284e027dd2591dc0b5b5ac111e394202cbf68f1
Edinho <git@final04.com>
2021-04-09 09:52:04 -0300

    indexing account.name

--- traded/db/models.py
+++ traded/db/models.py
@@ -8,7 +8,7 @@ class Account(Base):
     __tablename__ = "account"
 
     id = sa.Column(sa.Integer, primary_key=True, index=True)
-    name = sa.Column(sa.String, unique=True, index=False, nullable=False)
+    name = sa.Column(sa.String, unique=True, index=True, nullable=False)
     postable = sa.Column(sa.Boolean, nullable=False)
     is_active = sa.Column(sa.Boolean, default=True, nullable=False)
     parent_id = sa.Column(
