c4575a41240b8c88e1d83e86916b40704503712c
Edinho <git@final04.com>
2021-04-10 12:50:51 -0300

    adde db setup for testing purposes

--- traded/main.py
+++ traded/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+import sqlalchemy as sa
+from fastapi import FastAPI, Depends
 
+from . import db
 from . import account_2 as account
 from . import asset_2 as asset
+from .dependencies import get_session
 
 
 app = FastAPI()
@@ -10,6 +13,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"pi": "3.1415926535897932384626433"}
+
+
+@app.get("/db_setup")
+def db_setup(session: sa.orm.Session = Depends(get_session)):
+    db.main.setup(session)
 
 
 app.include_router(account.router)
