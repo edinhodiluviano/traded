f0c9a4f6fda08a9c4b2aaed8af4832b69f007beb
Edinho <git@final04.com>
2021-04-10 08:51:15 -0300

    added currencies

--- tests/unit/test_asset_2.py
+++ tests/unit/test_asset_2.py
@@ -1,17 +1,21 @@
+import uuid
+import copy
+
+
 def test_asset_get_all(client):
-    resp = client.get("/asset")
+    resp = client.get("/asset/a")
     assert resp.status_code == 200
     assets = resp.json()
     assert isinstance(assets, list)
 
 
 def test_asset_default_database(client):
-    resp = client.get("/asset")
+    resp = client.get("/asset/a")
     assert len(resp.json()) > 0
 
 
 def test_returned_assets_have_basic_fields(client):
-    resp = client.get("/asset")
+    resp = client.get("/asset/a")
     for asset in resp.json():
         assert isinstance(asset, dict)
         assert "name" in asset
@@ -21,22 +25,84 @@ def test_returned_assets_have_basic_fields(client):
 
 
 def test_get_by_id(client):
-    resp = client.get("/asset/1")
+    resp = client.get("/asset/a/1")
     assert resp.status_code == 200
     assert resp.json()["id"] == 1
 
 
 def test_get_by_id_with_str_as_id(client):
-    resp = client.get("/asset/xxxx")
+    resp = client.get("/asset/a/xxxx")
     assert resp.status_code == 422
 
 
 def test_get_by_id_with_negative_id(client):
-    resp = client.get("/asset/-1")
+    resp = client.get("/asset/a/-1")
     assert resp.status_code == 422
 
 
 def test_get_by_id_with_non_existing_id(client):
-    resp = client.get("/asset/9999999")
+    resp = client.get("/asset/a/9999999")
     assert resp.status_code == 200
     assert resp.json() is None
+
+
+def test_create_new_currency(client):
+    name = str(uuid.uuid4())
+    new_currency = dict(
+        name=name,
+        description="lcamds",
+        is_active=True,
+    )
+    resp = client.post("/asset/currency", json=new_currency)
+    assert resp.status_code == 200
+    assert "id" in resp.json()
+    assert "name" in resp.json()
+    assert resp.json()["name"] == name
+
+
+def test_create_new_currency_with_missing_data(client):
+    name = str(uuid.uuid4())
+    new_currency = dict(
+        name=name,
+        is_active=True,
+    )
+    resp = client.post("/asset/currency", json=new_currency)
+    assert resp.status_code == 422
+
+
+def test_create_new_currency_with_extra_data(client):
+    name = str(uuid.uuid4())
+    new_currency = dict(
+        name=name,
+        description="lcamds",
+        is_active=True,
+        bogus_field_123="cksdmcs",
+    )
+    resp = client.post("/asset/currency", json=new_currency)
+    assert resp.status_code == 422
+
+
+def test_get_all_currencies(client):
+    resp = client.get("/asset/currency")
+    assert resp.status_code == 200
+    assert isinstance(resp.json(), list)
+    assert len(resp.json()) > 5
+    for cur in resp.json():
+        assert "type" in cur
+        assert cur["type"] == "currency"
+
+
+def test_edit_currency(client):
+    name = str(uuid.uuid4())
+    new_curr = dict(name=name, description="", is_active=True)
+    resp = client.post("/asset/currency", json=new_curr)
+    new_curr = resp.json()
+    edited_curr = copy.deepcopy(new_curr)
+    edited_curr["description"] = "aaa"
+    del edited_curr["id"]
+    resp2 = client.put(f"asset/currency/{new_curr['id']}", json=edited_curr)
+    assert resp2.status_code == 200
+    assert isinstance(resp2.json(), dict)
+    for field in ["id", "name"]:
+        assert resp2.json()[field] == new_curr[field]
+    assert resp2.json()["description"] == "aaa"
--- traded/asset_2.py
+++ traded/asset_2.py
@@ -35,17 +35,66 @@ class Asset(_AssetBase):
         orm_mode = True
 
 
-@router.get("/", response_model=list[Asset])
+class CurrencyCreate(_AssetBase):
+    type: AssetTypes = AssetTypes.currency
+
+    class Config:
+        extra = "forbid"
+
+
+class Currency(CurrencyCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+@router.get("/a", response_model=list[Asset])
 def get_all(session: sa.orm.Session = Depends(get_session)):
     assets = session.query(models.Asset).all()
     return assets
 
 
-@router.get("/{asset_id}", response_model=Asset)
+@router.get("/a/{asset_id}", response_model=Asset)
 def get_by_id(
     asset_id: int = Path(..., ge=1),
     session: sa.orm.Session = Depends(get_session),
 ):
     query = session.query(models.Asset)
     asset = query.filter(models.Asset.id == asset_id).first()
+    return asset
+
+
+@router.post("/currency", response_model=Currency)
+def create_currency(
+    currency: CurrencyCreate,
+    session: sa.orm.Session = Depends(get_session),
+):
+    currency_db = models.Asset(**currency.dict())
+    session.add(currency_db)
+    session.commit()
+    session.refresh(currency_db)
+    return currency_db
+
+
+@router.get("/currency", response_model=list[Currency])
+def get_all_currencies(session: sa.orm.Session = Depends(get_session)):
+    query = session.query(models.Asset)
+    currencies = query.filter(models.Asset.type == AssetTypes.currency).all()
+    return currencies
+
+
+@router.put("/currency/{asset_id}", response_model=Currency)
+def update_currency(
+    currency: CurrencyCreate,
+    asset_id: int = Path(..., ge=1),
+    session: sa.orm.Session = Depends(get_session),
+):
+    asset = session.query(models.Asset).get(asset_id)
+    for field, value in currency.dict().items():
+        if field == "id":
+            continue
+        setattr(asset, field, value)
+    session.commit()
+    session.refresh(asset)
     return asset
