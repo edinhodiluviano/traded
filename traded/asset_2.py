9a75c4933040f6840ef4ff09a6c63c8184e213c3
Edinho <git@final04.com>
2021-04-11 13:46:42 -0300

    refactoring endpoints creation

--- traded/asset_2.py
+++ traded/asset_2.py
@@ -2,8 +2,8 @@ from enum import Enum
 import traceback as tb
 
 import sqlalchemy as sa
-from fastapi import APIRouter, Depends, Path, HTTPException
 from pydantic import BaseModel
+from fastapi import APIRouter, Depends, Path, HTTPException
 
 from .dependencies import get_session
 from .db import models
@@ -26,14 +26,31 @@ class _AssetBase(BaseModel):
     name: str
     description: str
     is_active: bool
-    type: AssetTypes
+
+    class Config:
+        extra = "forbid"
+        orm_mode = True
 
 
 class Asset(_AssetBase):
     id: int
+    type: AssetTypes
 
-    class Config:
-        orm_mode = True
+
+class CurrencyCreate(_AssetBase):
+    pass
+
+
+class Currency(_AssetBase):
+    id: int
+
+
+class StockCreate(_AssetBase):
+    pass
+
+
+class Stock(_AssetBase):
+    id: int
 
 
 @router.get("/", response_model=list[Asset])
@@ -53,86 +70,83 @@ def get_by_id(
     return asset
 
 
-class CurrencyCreate(_AssetBase):
-    type: AssetTypes = AssetTypes.currency
-
-    class Config:
-        extra = "forbid"
-
-
-class Currency(CurrencyCreate):
-    id: int
-
-    class Config:
-        orm_mode = True
-
-
-class StockCreate(_AssetBase):
-    type: AssetTypes = AssetTypes.stock
-
-    class Config:
-        extra = "forbid"
-
-
-class Stock(StockCreate):
-    id: int
-
-    class Config:
-        orm_mode = True
-
-
-@router.post("/currency", response_model=Currency)
-def create_currency(
-    currency: CurrencyCreate,
-    session: sa.orm.Session = Depends(get_session),
+def create_asset_factory(
+    asset_type: AssetTypes,
+    asset_create_class: _AssetBase,
 ):
-    currency_db = models.Asset(**currency.dict())
-    session.add(currency_db)
-    try:
-        session.commit()
-    except sa.exc.IntegrityError:
-        session.rollback()
-        raise HTTPException(status_code=422, detail=tb.format_exc(limit=0))
-    else:
-        session.refresh(currency_db)
-        return currency_db
+    def create_asset(
+        asset_create: asset_create_class,
+        session: sa.orm.Session = Depends(get_session),
+    ):
+        asset_dict = asset_create.dict()
+        asset_dict["type"] = asset_type.value
+        asset_db = models.Asset(**asset_dict)
+        session.add(asset_db)
+        try:
+            session.commit()
+        except sa.exc.IntegrityError:
+            session.rollback()
+            raise HTTPException(status_code=422, detail=tb.format_exc(limit=0))
+        else:
+            session.refresh(asset_db)
+            return asset_db
+
+    create_asset.__name__ = f"Create {asset_type.value.title()}"
+    create_asset.__doc__ = (
+        f"Creates an asset of type {asset_type.value.title()}"
+    )
+    return create_asset
 
 
-@router.post("/stock", response_model=Stock)
-def create_stock(
-    stock: StockCreate,
-    session: sa.orm.Session = Depends(get_session),
+def update_asset_factory(
+    asset_type: AssetTypes,
+    asset_create_class: _AssetBase,
 ):
-    stock_db = models.Asset(**stock.dict())
-    session.add(stock_db)
-    session.commit()
-    session.refresh(stock_db)
-    return stock_db
+    def update_asset(
+        asset_id: int,
+        asset_update: asset_create_class,
+        session: sa.orm.Session = Depends(get_session),
+    ):
+        asset_db = session.query(models.Asset).get(asset_id)
+        for field, value in asset_update.dict().items():
+            setattr(asset_db, field, value)
+        try:
+            session.commit()
+        except sa.exc.IntegrityError:
+            session.rollback()
+            raise HTTPException(status_code=422, detail=tb.format_exc(limit=0))
+        else:
+            session.refresh(asset_db)
+            return asset_db
+
+    update_asset.__name__ = f"Update {asset_type.value.title()}"
+    update_asset.__doc__ = f"Updates a single {asset_type.value.title()}"
+
+    return update_asset
 
 
-@router.put("/currency/{asset_id}", response_model=Currency)
-def update_currency(
-    currency: CurrencyCreate,
-    asset_id: int = Path(..., ge=1),
-    session: sa.orm.Session = Depends(get_session),
+def endpoints_factory(
+    asset_type: AssetTypes,
+    asset_create_class: _AssetBase,
+    response_model: _AssetBase,
 ):
-    asset = session.query(models.Asset).get(asset_id)
-    for field, value in currency.dict().items():
-        setattr(asset, field, value)
-    session.commit()
-    session.refresh(asset)
-    return asset
+
+    # create endpoint
+    endpoint = f"/{asset_type.value}"
+    create_func = create_asset_factory(
+        asset_type=asset_type,
+        asset_create_class=asset_create_class,
+    )
+    router.post(endpoint, response_model=response_model)(create_func)
+
+    # update endpoint
+    endpoint = f"/{asset_type.value}/{{asset_id}}"
+    update_func = update_asset_factory(
+        asset_type=asset_type,
+        asset_create_class=asset_create_class,
+    )
+    router.put(endpoint, response_model=response_model)(update_func)
 
 
-@router.put("/stock/{asset_id}", response_model=Stock)
-def update_stock(
-    stock: StockCreate,
-    asset_id: int = Path(..., ge=1),
-    session: sa.orm.Session = Depends(get_session),
-):
-    asset = session.query(models.Asset).get(asset_id)
-    for field, value in stock.dict().items():
-        setattr(asset, field, value)
-    session.commit()
-    session.refresh(asset)
-    return asset
+endpoints_factory(AssetTypes.currency, CurrencyCreate, Currency)
+endpoints_factory(AssetTypes.stock, CurrencyCreate, Currency)
