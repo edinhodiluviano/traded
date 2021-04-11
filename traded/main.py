c586a3d9573e4fe5686a5a2f8e1fb664ec26d88b
Edinho <git@final04.com>
2021-04-11 14:00:11 -0300

    docstrings

--- traded/main.py
+++ traded/main.py
@@ -12,11 +12,17 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    "Try me out. I do no harm (and no good either)"
     return {"pi": "3.1415926535897932384626433"}
 
 
 @app.get("/db_setup")
 def db_setup(session: sa.orm.Session = Depends(get_session)):
+    """
+    ### test only
+    Setups the database tables and some values for testing purpuses.<br>
+    This endpoint shreds your database and data when used.
+    """
     db.main.setup(session)
 
 
